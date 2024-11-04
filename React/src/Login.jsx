c024a506080e408b011c8870f0c3fb2e09c3044e
Jax Choong <choongjax@gmail.com>
2024-11-04 18:38:42 +0800

    style: login form is centered

--- React/src/Login.jsx
+++ React/src/Login.jsx
@@ -4,21 +4,23 @@ import "./styles.css";
 
 export default function Login(){
     return(
-        <Container  fluid= 'true' centerContent = 'true' className='loginPrompt'>
-            <h1>Login</h1>
-            <form>
-                <label>
-                    Username:
-                    <input type="text" name="username" />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" name="password" />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-        </Container>
+        <div className='parentContainer'>
+            <Container  fluid= 'true' centerContent = 'true' className='loginPrompt'>
+                <h1>Login</h1>
+                <form>
+                    <label>
+                        Username:
+                        <input type="text" name="username" />
+                    </label>
+                    <br />
+                    <label>
+                        Password:
+                        <input type="password" name="password" />
+                    </label>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+            </Container>
+        </div>
     )
 };
