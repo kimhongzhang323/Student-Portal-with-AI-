7acdecac55bb70cc5a9bd9b869602143b20c6c97
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2024-11-04 15:24:11 +0800

    Removed api key

--- AI_Student_Portal/AI_Student_Portal/settings.py
+++ AI_Student_Portal/AI_Student_Portal/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # settings.py
-GEMINI_API_KEY = 'AIzaSyDD0546soE4dGDrG7FMTQz_ieAO9mSB-7k'
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
