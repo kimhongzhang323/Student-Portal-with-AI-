df693dd6716d2b716d60619899f258959fbcf389
Jax Choong <choongjax@gmail.com>
2024-11-05 17:32:38 +0800

    feat: now has login button and signup link, along with forgot password link

--- React/src/Login.jsx
+++ React/src/Login.jsx
@@ -28,21 +28,41 @@ export default function Login(){
                             <Field.Label css={floatingStyles}>Password</Field.Label>
                         </Box>
                     </Field.Root> */}
-                    <div className='loginInputs' style={{width:'100%'}}>
-                        <Input type='text' placeholder='Username' style={{margin:'10px auto',width:'70%'}}/>
-                        <div className='centeredContainer' style={{width:'70%'}}>
-                            <PasswordInput style={{margin:'auto'}} placeholder="Password" />
-                        </div>      
+                    <div className='loginInputs centeredContainer' style={{flexDirection:'column'}}>
+                        <div style={{width:'90%'}}>
+                            <Heading size='md' style={{margin:'0 auto',padding:'0',textAlign:'left'}}>Email</Heading>
+                            <Input type='text' placeholder='abc@example.com' style={{margin:'5px auto 15px',width:'100%'}}/>
+                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                <Heading size='md' style={{ margin: '0', padding: '0', textAlign: 'left' }}>Password</Heading>
+                                <a href='/forgotPassword' style={{ fontSize: '0.8rem', textDecoration: 'underline', fontWeight: '500', margin: '0' }}>
+                                    Forgot your password?
+                                </a>
+                            </div>
+                            <div className='centeredContainer' style={{width:'100%',margin:'5px auto'}}>
+                                <PasswordInput style={{margin:'0 auto'}}/>
+                                
+                            </div>
+                        </div>     
                     </div>
-                    <div className='centeredContainer'>
-                        <p style={{fontSize:'0.8rem',fontWeight:'bolder',color:'rgba(0,0,0,0.5'}}> Or continue with</p>
-                        <div style={{width:'60%',borderBottom:'1px solid rgba(0,0,0,0.3)',marginLeft:'3%'}}></div>
-                    </div>
+                    <Button variant='subtle' type='submit' style={{width:'90%'}}>
+                        Log In
+                    </Button>
+                </form>
+                <div className='centeredContainer' style={{width:'100%'}}>
+                    <p style={{fontSize:'0.8rem',fontWeight:'bolder',color:'rgba(0,0,0,0.5'}}> Or continue with</p>
+                    <div style={{width:'50%',borderBottom:'1px solid rgba(0,0,0,0.3)',marginLeft:'3%'}}></div>
+                </div>
+                <form>
                     <Button  colorPalette='gray' type='submit' variant={'outline'} className='hoverButton' style={{borderRadius:'30px',padding:'0'}}> 
                         <img src="/googlelogo.svg" alt="google" className='googleTextLogo' style={{width:'100%',height:'100%',margin:'0',padding:'0'}}/>
                     </Button>
-                    
                 </form>
+                <div className='centeredContainer' style={{width:'100%'}}>
+                    <p style={{fontSize:'0.9rem',fontWeight:'500',color:'rgba(0,0,0,0.8)'}}> Don't have an account?</p>
+                    <a style={{ fontSize: '0.9rem', textDecoration: 'underline', fontWeight: '600', marginLeft: '4px' }} href='/signUp'>
+                        Sign up
+                    </a>
+                </div>
             </Container>
         </div>
     )
