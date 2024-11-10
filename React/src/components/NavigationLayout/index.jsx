da1bc657afaf31a16c3338fe11743ca3f43748ee
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-11-10 12:09:01 +0800

    feat: create navigation layout wrapper

---
+++ React/src/components/NavigationLayout/index.jsx
@@ -0,0 +1,21 @@
+import { Box } from "@chakra-ui/react";
+import { NavigationBar } from "../NavigationBar";
+import { SideBar } from "../Sidebar";
+
+export function NavigationLayout(props) {
+  return (
+    <>
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <NavigationBar />
+        <Box display="flex" flex="1">
+          <SideBar />
+          <Box display="flex" flex="1">
+            <Box margin={"40px"} width="100%">
+              {props.children}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  )
+}
---
+++ React/src/components/OverlaySidebarv2/index.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import './sidebar.css';
+import HamburgerLogo from '../../assets/hamburger.svg'
+import MyPortalLogo from '../../assets/MyPortal.svg'
+import CoursesPageSVG from '../../assets/CoursesPage.svg'
+import GeminiPageSVG from '../../assets/GeminiPage.svg'
+import HomePageSVG from '../../assets/HomePage.svg'
+import PagesPageSVG from '../../assets/PagesPage.svg'
+import SettingsPageSVG from '../../assets/SettingsPage.svg'
+import CalendarPageSVG from '../../assets/calendar.svg'
+
+import { Button } from '@chakra-ui/react';
+
+export function OverlaySidebar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+  return (
+    <>
+    <div>
+      <Button className="open-btn" onClick={toggleSidebar}>
+        <img src={HamburgerLogo} width="28" alt="" style={{"marginRight": "15px"}} />
+      </Button>
+
+      <div className={`overlay ${isOpen ? 'active' : ''}`} onClick={toggleSidebar}></div>
+
+      <div className={`sidebar ${isOpen ? 'active' : ''}`}>
+        <button className="close-btn" style={{"color":"black"}} onClick={toggleSidebar}>
+          &times;
+        </button>
+        <img src={MyPortalLogo} width="120px" alt="" />
+        <ul style={{"marginTop": "25px"}}>
+          <li>
+            <img src={HomePageSVG} alt="" />
+            <a href="#home" style={{"color":"black"}}>Dashboard</a>
+          </li>
+          <li>
+            <img src={CalendarPageSVG} alt="" />
+            <a href="#services" style={{"color":"black"}}>Calendar</a>
+          </li>
+          <li>
+            <img src={PagesPageSVG} alt="" />
+            <a href="#contact" style={{"color":"black"}}>Pages</a>
+          </li>
+          <li>
+            <img src={GeminiPageSVG} alt="" />
+            <a href="#about" style={{"color":"black"}}>Gemini</a>
+          </li>
+          <li>
+            <img src={CoursesPageSVG} alt="" />
+            <a href="#about" style={{"color":"black"}}>Courses</a>
+          </li>
+          <li>
+            <img src={SettingsPageSVG} alt="" />
+            <a href="#about" style={{"color":"black"}}>Settings</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
