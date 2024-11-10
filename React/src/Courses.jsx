eb7ea5d8491d56d446a121974429517aa22b5fff
Jax Choong <choongjax@gmail.com>
2024-11-10 15:58:04 +0800

    feat: now has subject title section

--- React/src/Courses.jsx
+++ React/src/Courses.jsx
@@ -1,15 +1,21 @@
+import { Heading } from '@chakra-ui/react'
 import { NavigationLayout } from './components/NavigationLayout'
+import { FaRegBell } from "react-icons/fa";
+import { TbHandClick } from "react-icons/tb";
 
 export default function Courses(){
     return(
         <NavigationLayout>
-                <h1>Courses</h1>
-                <p>Here are the courses we offer:</p>
-                <ul>
-                    <li>React</li>
-                    <li>Vue</li>
-                    <li>Angular</li>
-                </ul>
+            <div className='centeredContainer'>
+                <div className='subjectTitle'>
+                    <Heading size='6xl' style={{width:'80%',textAlign:'left',textWrap:'unset'}}>Object Oriented Programming</Heading>
+                    <div className='shortAnnouncement'>
+                        <FaRegBell style={{height:'30px',width:'30px',margin:'5px 10px'}}/>
+                        <strong>Announcement: Lorem Ipsum Dolor sit Amet</strong>
+                        <TbHandClick className='handClickIcon'/>
+                    </div>
+                </div>
+            </div>
         </NavigationLayout>
     )
 }
