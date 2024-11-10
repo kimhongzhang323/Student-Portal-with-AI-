1b5feda936e6d9e5f15b8c48cd6f2d49f847305f
Jax Choong <choongjax@gmail.com>
2024-11-10 15:34:42 +0800

    feat: now has navbar in courses

--- React/src/Courses.jsx
+++ React/src/Courses.jsx
@@ -1,15 +1,15 @@
-
+import { NavigationLayout } from './components/NavigationLayout'
 
 export default function Courses(){
     return(
-        <div>
-            <h1>Courses</h1>
-            <p>Here are the courses we offer:</p>
-            <ul>
-                <li>React</li>
-                <li>Vue</li>
-                <li>Angular</li>
-            </ul>
-        </div>
+        <NavigationLayout>
+                <h1>Courses</h1>
+                <p>Here are the courses we offer:</p>
+                <ul>
+                    <li>React</li>
+                    <li>Vue</li>
+                    <li>Angular</li>
+                </ul>
+        </NavigationLayout>
     )
 }
