793b8b0fd9f60bd211b1ef310422399563c0a0de
Jax Choong <choongjax@gmail.com>
2024-11-13 15:43:33 +0800

    feat: now has assignment submission items

--- React/src/components/WeeklyContainer/index.jsx
+++ React/src/components/WeeklyContainer/index.jsx
@@ -8,8 +8,9 @@ export function WeeklyContainer(props) {
             {props.week}
         </Heading>
         <div style={{width:'calc(100% - 5px)',borderBottom:'1.5px solid rgba(0,0,0,0.3)',margin:'5px'}}></div>
-        <WeeklyItem title='BEFORE YOU COME TO CLASS' content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ullamcorper turpis, finibus porttitor enim. Aliquam malesuada velit aliquet odio tristique, euismod aliquet mauris consequat. " />
-        <WeeklyItem title='DURING CLASS' content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ullamcorper turpis, finibus port titor enim. Aliquam malesuada velit aliquet odio tristique, euismod aliquet mauris consequat. " />
+        <WeeklyItem title='BEFORE YOU COME TO CLASS' type='general' content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ullamcorper turpis, finibus porttitor enim. Aliquam malesuada velit aliquet odio tristique, euismod aliquet mauris consequat. " />
+        <WeeklyItem title='DURING CLASS' type='general' content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ullamcorper turpis, finibus port titor enim. Aliquam malesuada velit aliquet odio tristique, euismod aliquet mauris consequat. " />
+        <WeeklyItem title="ASSIGNMENT SUBMISSION" type='assignmentUpload' content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis ullamcorper turpis, finibus porttitor enim. Aliquam malesuada velit aliquet odio tristique, euismod aliquet mauris consequat. " />
     </div>
   );
 }
--- React/src/components/WeeklyItem/index.jsx
+++ React/src/components/WeeklyItem/index.jsx
@@ -1,12 +1,22 @@
 import { Heading } from "@chakra-ui/react";
 import { CiGlobe } from "react-icons/ci";
+import { MdFileUpload } from "react-icons/md";
 
 export function WeeklyItem(props){
+    let icon,color;
+    if (props.type === 'general'){
+        icon = <CiGlobe style={{height:'50px',width:'50px',padding:'5px'}}/>
+        color = "rgb(0,140,255)"
+    }
+    else if (props.type === 'assignmentUpload'){
+        icon = <MdFileUpload style={{height:'50px',width:'50px',padding:'5px'}}/>
+        color = "rgb(255,159,63)"
+    }
     return(
         <div className='weeklyItem'>
             <div className="itemTitle">
-                <div className="generalAnnouncement">
-                    <CiGlobe style={{height:'50px',width:'50px',padding:'5px'}}/>
+                <div className="itemIcon" style={{backgroundColor: color}}>
+                    {icon}
                 </div>
                 <Heading style={{textAlign:'left',paddingLeft:'10px'}}>
                     {props.title}
