b308d477281510c1d5f0fc5983956719f4b8b902
Jax Choong <choongjax@gmail.com>
2024-11-15 11:24:49 +0800

    feat: now has boilerplate files page

--- React/src/LectureNotes.jsx
+++ React/src/LectureNotes.jsx
@@ -1,9 +1,26 @@
 import { NavigationLayout } from './components/NavigationLayout'
+import { CiFolderOn } from "react-icons/ci";
+import { Heading } from '@chakra-ui/react';
+import { FaFolderTree } from "react-icons/fa6";
+import { FaRegFolderOpen } from "react-icons/fa";
 
 export default function LectureNotes(props){
     return(
         <NavigationLayout>
+            <div className='folderTitle'>
+                <CiFolderOn size='70px'/>
+                <Heading style={{marginLeft:'10px'}}>
+                    Lecture Notes
+                </Heading>
+            </div>
+            <div className='filesContainer'>
+                <FaRegFolderOpen  size='40px'  style={{marginLeft:'30px', marginTop:'20px'}}/>
+                <div style={{display:'flex', height:'100%', alignItems:'stretch'}}>
+                    <div style={{height:'70%',borderRight:'3px solid rgba(0,0,0,0.3)', marginLeft:'40px', position:'absolute'}}>
 
+                    </div>
+                </div>
+            </div>
         </NavigationLayout>
     )
 }
