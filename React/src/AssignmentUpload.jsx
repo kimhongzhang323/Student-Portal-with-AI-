ef58139966c7ed33ba247ddb84a3f87436f3209b
Jax Choong <choongjax@gmail.com>
2024-11-16 17:53:20 +0800

    feat: now has add submission title and button

--- React/src/AssignmentUpload.jsx
+++ React/src/AssignmentUpload.jsx
@@ -2,6 +2,7 @@ import { NavigationLayout } from './components/NavigationLayout'
 import { Heading } from '@chakra-ui/react';
 import { MdOutlineFileUpload } from "react-icons/md";
 import AssignmentDetailsGroup from './components/AssignmentDetailsGroup';
+import {Button} from '@chakra-ui/react';
 
 export default function LectureNotes(props){
     return(
@@ -15,6 +16,10 @@ export default function LectureNotes(props){
             <div className='filesContainer'>
                 <div className='centeredContainer'>
                     <AssignmentDetailsGroup />
+                </div>
+                <div style={{display:'flex',alignItems:'center'}}>
+                    <Heading style={{margin:'auto 20px'}} size='3xl'>Submissions</Heading>
+                    <Button style={{color:'white',fontSize:'0.9em',borderRadius:'10px'}} bgColor={'rgb(52,147,255)'}>Add Submission</Button>
                 </div>
                 
             </div>
